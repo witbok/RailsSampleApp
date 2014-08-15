696f72ef7b537dc8aeb79a84a57d37f8075d7a0d
Jess <witbok@gmail.com>
2014-08-14 20:25:20 -0400

    Fleshed out layout and refactored tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 SampleApp::Application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
+  root 'static_pages#home'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -45,7 +44,7 @@ SampleApp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
