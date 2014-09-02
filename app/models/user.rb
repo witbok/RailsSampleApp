ef282b4e536dc58809e9e34048f2b6bac1c80ada
Jess <witbok@gmail.com>
2014-09-02 18:25:25 -0400

    Added Sign In Features :

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -10,4 +11,18 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
